a5ed666ba6e1216d9713f3909b0e75d6445ddc22
Jaringvm <1009853@hr.nl>
2021-03-06 21:17:41 +0100

    Roles Added(fix)

--- Inlogscherm/Program.cs
+++ Inlogscherm/Program.cs
@@ -10,29 +10,34 @@ namespace cinema
             string pw_strenght = "[Missing]                     ";
             while (pw == true)
             {
+                // let's you put in a password and checks if it has an digit and a uppercase letter in it.
                 Console.Write(pw_strenght);
                 int digit = 0, letter = 0;
                 Console.Write("Give an 8 or longer password with a uppercase letter and a Number: ");
                 string password = Console.ReadLine();
                 for (int c = 0; c < password.Length; c++)
                 {
+                    // check uppercase letter
                     if (char.IsUpper(password, c))
                     {
                         letter = 1;
                         pw_strenght = "[Missing a Digit]               ";
                     }
+                    // check digit 
                     if (char.IsDigit(password, c))
                     {
                         digit = 1;
                         pw_strenght = "[Missing a Uppercase Letter]  ";
                     }
                 }
+                // checks if the password is 8 or longer
                 if (password.Length < 8)
                 {
                     pw = true;
                     pw_strenght = "[your password is to short]    ";
 
                 }
+                // check if you are sure you want to use this as your password 
                 if (password.Length >= 8 && (digit == 1 && letter == 1))
                 {
                     Console.WriteLine("The Password is strong.");
@@ -48,8 +53,28 @@ namespace cinema
                         pw_strenght = "[Missing Digit and Uppercase] ";
 
                     }
-
                 }
+            }
+            // Give an account a role, later on we can put varriables which are only true for a specific account role
+            int Account_Number = 0;
+            string Account_Role = "";
+            switch (Account_Number)
+            {
+                case 1:
+                    Account_Role = "Guest";
+                    break;
+
+                case 2:
+                    Account_Role = "User";
+                    break;
+
+                case 3:
+                    Account_Role = "Employe";
+                    break;
+
+                case 4:
+                    Account_Role = "Moderator";
+                    break;
             }
 
         }
