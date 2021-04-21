4998c0c83cd5cac81bc832ed2c005e4279f97f85
Pemseles <TJBouwsema@gmail.com>
2021-04-21 17:28:22 +0200

    Beginnetje met zoeken
    - Relative filepath (hoef je niet de exacte filepath hebben)
    - Nieuwe class met zoekinput (doen we later meer mee)
    - Super veel testen

--- Inlogscherm/Zoekfunctie.cs
+++ Inlogscherm/Zoekfunctie.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using Newtonsoft.Json;
+using System.IO;
+using static System.Console;
 
 
 namespace ConsoleApp1
@@ -23,4 +25,40 @@ namespace ConsoleApp1
         // deze class heeft alle objects van de Film class
         public Film[] FilmArray { get; set; }
     }
+    // vraag user input aan (leeftijd, director, naam of genre)
+    // maak een for loop aan die door de JSON op zoek gaat
+    // wanneer input overeenkomt met iets uit de lijst, return film info ( leeftijdkwalificatie, director, genre, naam )
+    // vergeet niet dat alle overeenkomende input weergegeven moet worden
+    // optie leeftijdskwalificatie : geef films met overeenkomende leeftijd plus lager > vragen PO? waarschijnlijk zegt hij optie voor allebei vast en zeker, hoe vervelend ook
+    // een check maken of nummers in de input zitten > zo ja omzetten naar int
+
+    // string[] numbersInputArray = { "0" , "6" , "9" , "12" , "14" , "16" , "18"};
+    // string array met mogelijke kwalififacties leeftijd voor vergelijking met input
+
+    // for (i = 0 ; i < numbersInputArray.Length ; i++){
+    // if(inputZoek == numbersInputArray[i]){
+    // int inputLeeftijd = Convert.ToInt32(Console.ReadLine());
+    // }
+    // input naam veranderd voor leeftijdskwalificatie 
+
+    // misschien eerst vragen wat voor type input het is?
+    // console writeline is volgens mij standaard string
+    // ik bedoel genre/ director/ naam/ leeftijd > anders aan natnael vragen
+    // zou kunnen? maar t loopt door t hele json heen en moet de volledige info ophalen
+
+    class Zoekfunctie
+    {
+        public string InputZoek;
+
+        public Zoekfunctie(string userInput)
+        {
+            this.InputZoek = userInput;
+        }
+
+        public void ZoekMethod()
+        {
+            this.InputZoek = InputZoek + " testetstetsttettstettste";
+        }
+    }
+
 }
