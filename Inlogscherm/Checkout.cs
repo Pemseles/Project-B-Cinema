2d7adb64453a224c349b0673d815b96a05f37225
Pemseles <TJBouwsema@gmail.com>
2021-05-15 16:27:09 +0200

    afrekenscherm af totdat de rest klaar is
    nog moet doen
    - linken aan filmscherm
    - linken aan productenlijst
    - linken aan zaal functie

--- Inlogscherm/Checkout.cs
+++ Inlogscherm/Checkout.cs
@@ -42,15 +42,24 @@ namespace ConsoleApp1
             {
                 List<string> paymentScreenLayout = new List<string>()
                 {
-                    "[ Keuze aanpassen ]" ,
+                    "[ Keuze resetten  ]" ,
                     "[    Afrekenen    ]" ,
                     "[   VIP Upgrade   ]" ,
                     "[      Terug      ]"
                 };
 
-                SelectedOption = PaymentScreenSelect(paymentScreenLayout);
+                SelectedOption = PaymentScreenSelect(paymentScreenLayout, SelectedOption);
+                if (SelectedOption == "[    Afrekenen    ]")
+                {
+                    Console.Clear();
+                    CheckoutScreen();
 
-                if (SelectedOption == "[      Terug      ]")
+                }
+                else if (SelectedOption == "[   VIP Upgrade   ]")
+                {
+                    // doe hier VIP upgraden
+                }
+                else if (SelectedOption == "[      Terug      ]")
                 {
                     PaymentScreenBool = false;
                 }
@@ -59,7 +68,7 @@ namespace ConsoleApp1
             MainMenu.Mainmenu();
         }
 
-        private string PaymentScreenSelect(List<string> items)
+        private string PaymentScreenSelect(List<string> items, string SelectedItem)
         {
             Console.Clear();
             DrawCart();
@@ -80,6 +89,7 @@ namespace ConsoleApp1
                 }
                 Console.ResetColor();
             }
+            DrawCheckouts(SelectedItem);
             ConsoleKeyInfo ckey = Console.ReadKey();
             if (ckey.Key == ConsoleKey.DownArrow)
             {
@@ -141,6 +151,85 @@ namespace ConsoleApp1
                 }
             }
             Console.WriteLine(cartString);
+        }
+
+        private void ResetChoices()
+        {
+            if (this.FilmIndex >= 0)
+            {
+                UpdateFilmIndex(-1);
+            }
+            if (this.Products.Count > 0)
+            {
+                UpdateProducts(new List<int>());
+            }
+            if (this.Seats.Count > 0)
+            {
+                UpdateSeats(new List<int>());
+            }
+        }
+
+        public void CheckoutScreen()
+        {
+            index = 0;
+            bool checkoutBool = true;
+            string SelectedOption = "";
+            while (checkoutBool)
+            {
+                List<string> checkoutScreenLayout = new List<string>()
+                {
+                    "[      iDeal      ]" ,
+                    "[   Credit card   ]" ,
+                    "[     Contant     ]" ,
+                    "[      Terug      ]"
+                };
+                SelectedOption = PaymentScreenSelect(checkoutScreenLayout, SelectedOption);
+                
+                if (SelectedOption == "[      Terug      ]")
+                {
+                    checkoutBool = false;
+                }
+                else
+                {
+                    DrawCheckouts(SelectedOption);
+                }
+            }
+            Console.Clear();
+            PaymentScreen();
+        }
+
+        private void DrawCheckouts(string selectedOption)
+        {
+            if (selectedOption == "[     Contant     ]" || selectedOption == "[      iDeal      ]" || selectedOption == "[   Credit card   ]")
+            {
+                if (this.FilmIndex < 0)
+                {
+                    Console.WriteLine("\n                                 [       U heeft nog geen film uitgekozen       ]");
+                    Console.WriteLine("                                 [          U kunt nog niet afrekenen           ]\n");
+                }
+                else
+                {
+                    if (selectedOption == "[     Contant     ]")
+                    {
+                        // tel de prijs op als contant geselecteerd is
+                    }
+                    else
+                    {
+                        Console.WriteLine("                                 [  Transactie gelukt, bedankt voor uw aankoop  ]\n");
+                    }
+                }
+            }
+            else if (selectedOption == "[ Keuze resetten  ]")
+            {
+                if (this.FilmIndex < 0 && this.Products.Count < 1 && this.Seats.Count < 1)
+                {
+                    Console.WriteLine("\n                                 [            Uw winkelwagen is leeg            ]\n");
+                }
+                else
+                {
+                    ResetChoices();
+                }
+            }
         }
     }
 }
