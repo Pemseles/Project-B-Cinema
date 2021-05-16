90e06850ec6b59e5623677cbaf3f6ac190d16e8c
John <74536719+johnnyboy-tech@users.noreply.github.com>
2021-05-16 12:56:07 +0200

    I hate github

---
+++ Inlogscherm/Filmlist.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    public class Movielist
+    {
+        public string name { get; set; }
+        public Movie[] movies { get; set; }
+    }
+
+    public class Movie
+    {
+        public string id { get; set; }
+        public string moviename { get; set; }
+        public string director { get; set; }
+        public string[] cast { get; set; }
+        public string date { get; set; }
+        public string starttime { get; set; }
+        public string endtime { get; set; }
+        public int roomid { get; set; }
+        public string screentype { get; set; }
+        public string[] genre { get; set; }
+    }
+
+    public class filmmenu
+    {
+        public static Movie Filmmenu(string[] items, int index = 0, bool done = false)
+        {
+            string moviesjson = File.ReadAllText(Path.GetFullPath(@"movies.json"));
+            var movielist = JsonSerializer.Deserialize<Movielist>(moviesjson);
+            while (!done)
+            {
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if (i == index)
+                    {
+                        Console.Write("                                                ");
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine(items[i]);
+                    }
+                    else
+                    {
+                        Console.Write("                                                ");
+                        Console.WriteLine(items[i]);
+                    }
+                    Console.ResetColor();
+                }
+
+                ConsoleKeyInfo ckey = Console.ReadKey();
+                if (ckey.Key == ConsoleKey.DownArrow)
+                {
+                    if (index == items.Length - 1)
+                    {
+                        index = 0;
+                    }
+                    else { index++; }
+                }
+                else if (ckey.Key == ConsoleKey.UpArrow)
+                {
+                    if (index <= 0)
+                    {
+                        index = items.Length - 1;
+                    }
+                    else { index--; }
+                }
+                else if (ckey.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Wilt u {items[index]} selecteren? (j/n)");
+                    var input = Console.ReadLine();
+                    if (input == "j" || input == "J")
+                    {
+                        Console.Clear();
+                        done = true;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                    }
+                }
+
+                Console.Clear();
+            }
+            return movielist.movies[index]; 
+        }
+        
+    }
+
+}
---
+++ Inlogscherm/mainmenu.cs
@@ -0,0 +1,189 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static System.Console;
+using System.IO;
+using static ConsoleApp1.MainMenu;
+using static ConsoleApp1.Registers;
+// restricties opzetten voor gebruik van alleen backspace en enter anders redrawd ie m 
+namespace ConsoleApp1
+{
+    public class MainMenu
+    {
+        private static int index = 0;
+
+            private static string MainScreen(List<string> items)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (i == index)
+                {
+                    Console.Write("                                                ");
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine(items[i]);
+                }
+                else
+                {
+                    Console.Write("                                                ");
+                    Console.WriteLine(items[i]);
+                }
+                Console.ResetColor();
+            }
+
+            ConsoleKeyInfo ckey = Console.ReadKey();
+            if (ckey.Key != ConsoleKey.DownArrow || ckey.Key != ConsoleKey.UpArrow || ckey.Key != ConsoleKey.Enter)
+            {
+                
+            }
+            if (ckey.Key == ConsoleKey.DownArrow)
+            {
+                if (index == 8)
+                {
+                    index = 0;
+                }
+                else { index++; }
+            }
+            else if (ckey.Key == ConsoleKey.UpArrow)
+            {
+                if (index <= 0)
+                {
+                    index = 8;
+                }
+                else { index--; }
+            }
+            else if (ckey.Key == ConsoleKey.Enter)
+            {
+                return items[index];
+            }
+            else
+            {
+                return "";
+            }
+            Console.Clear();
+            return "";
+        }
+        public static void back()
+        {
+            ConsoleKeyInfo ckey = Console.ReadKey();
+            if (ckey.Key == ConsoleKey.Backspace)
+            {
+                Console.Clear();
+                Mainmenu();
+            } 
+        }
+        public static void Mainmenu()
+        {           
+            bool mainmenubool = true;
+            while (mainmenubool == true)
+            {
+                List<string> Mainscreen = new List<string>() {
+                "[     Zoek films   ]" ,
+                "[     Alle films   ]" ,
+                "[Hapjes en drankjes]" ,
+                "[  Informatiemenu  ]" ,
+                "[    Review menu   ]" ,
+                "[    Winkelmand    ]" ,
+                "[    VIP pagina    ]" ,
+                "[       Zalen      ]" ,
+                "[     Uitloggen    ]"
+                 };
+                // kijkt bij welke index de user zich bevind
+                string selectedMenuItem = MainScreen(Mainscreen);
+                if (selectedMenuItem == "[     Zoek films   ]")
+                {
+                    Console.Clear();
+                    
+                    Console.WriteLine("Dit is de zoekbalk");                    
+                    // kortere reference werkt niet : argument toevoegen
+                    //ConsoleApp1.SearchClass.FilmSearch();
+                    string filmJSONPath = Path.GetFullPath(@"FilmList.json");
+                    string jsonStringFilmList = File.ReadAllText(filmJSONPath);
+
+                    FilmArr filmList = new ConsoleApp1.FilmArr();
+                    filmList = JsonSerializer.Deserialize<ConsoleApp1.FilmArr>(jsonStringFilmList);
+                    //Console.WriteLine(filmLijst.FilmArray[1].Name);
+                    back();
+                    Console.Write("Geef hier op wat u zoekt :");
+                    string searchClassInput = Console.ReadLine();
+                    ConsoleApp1.SearchClass search1 = new ConsoleApp1.SearchClass(searchClassInput);
+                    List<ConsoleApp1.Film> searchList = search1.FilmSearch(filmList);
+                    string searchListString = search1.FilmLengthCheck(searchList);
+                    Console.WriteLine(searchListString);
+                    back();
+
+                }
+                if (selectedMenuItem == "[     Alle films   ]")
+                {
+                    Console.Clear();
+
+                    string moviesjson = File.ReadAllText(Path.GetFullPath(@"movies.json"));
+                    var movielist = JsonSerializer.Deserialize<Movielist>(moviesjson);
+                    string[] FilmList = new string[movielist.movies.Length];
+                    for (int i = 0;i<movielist.movies.Length;i++)
+                    {
+                        FilmList[i] = movielist.movies[i].moviename;
+                    }
+                    var selectedmovie = filmmenu.Filmmenu(FilmList);
+                    Console.WriteLine(selectedmovie.id + selectedmovie.moviename + selectedmovie.date);
+                    Console.ReadLine();
+                }
+                else if (selectedMenuItem == "[Hapjes en drankjes]")
+                {
+                    Console.Clear();
+                    ConsoleApp1.Products.Productmenu();
+                    back();
+                    // open de snacks json
+                }
+                else if (selectedMenuItem == "[  Informatiemenu  ]")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Dit is het informatie menu");
+                    back();
+                    // open de info panels
+                }
+                else if (selectedMenuItem == "[    Review menu   ]")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Dit is het review menu");
+                    back();
+                    // open de review list of json
+                }
+                else if (selectedMenuItem == "[    Winkelmand    ]")
+                {
+                    Console.Clear();
+                    // open de seats json + snackselected json
+                    // if email == seats.email && email == snacksselected.email
+                    // zo misschien info ophalen per account
+                }
+                else if (selectedMenuItem == "[    VIP pagina    ]")
+                {
+                    Console.Clear();
+                    Console.WriteLine("dit is de VIP pagina");
+                    back();
+                    // if account registratie == vip 
+                    // kan dit geopend worden 
+                }
+                else if (selectedMenuItem == "[       Zalen      ]")
+                {
+                    Console.Clear();
+                    Registers.moviehall();
+                    back();
+                    Console.SetWindowSize(120, 30);
+                    // if account registratie == vip 
+                    // kan dit geopend worden 
+                }
+                else if (selectedMenuItem == "[     Uitloggen    ]")
+                {
+                    Console.Clear();
+                    mainmenubool = false;   
+                }
+                Console.Clear();
+            }
+            
+        }
+    }
+}
